e6b5811e3ab6148ed41c430465977bb3d2ac284f
reversearrow <reversearrow@gmail.com>
2017-05-02 20:53:18 -0400

    Updated Scripts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
-elem = document.getElementById("cat")
+
 //console.log(elem);
+
+//Creating an Object called cat
+
+var cats = {
+  cats: [
+    {
+      "name" : "cat1",
+      "img": "img/img1.jpg"
+    },
+    {
+      "name": "cat2",
+      "img": "img/img2.jpg"
+    }
+  ]
+}
+
+var createElements = function() {
+  var header = document.getElementById("header");
+  for(i=0;i<cats.cats.length;i++) {
+    var catHeader = document.createElement("h1")
+    catHeader.id = "cat"
+    catHeader.innerHTML = cats["cats"][i].name
+    var image = document.createElement("img")
+    image.src = cats["cats"][i].img
+    var para = document.createElement("p")
+    para.id = "clickcounts"
+    header.appendChild(catHeader)
+    header.appendChild(image)
+    header.appendChild(para)
+  }
+}
+
+createElements()
+
+elem = document.getElementById("cat")
 
 var catClicks = function (){
   this.currentClicks= 1
@@ -17,8 +52,3 @@ elem.addEventListener('click', function() {
   clicks.innerHTML = innerText;
   countClicks.addClick();
 });
-
-var cats = {
-  name: "Cat1",
-  pic: ""
-}
